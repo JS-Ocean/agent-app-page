6931e4f3740741310639be5eecd01999301e053a
Vlad Pekh <volodymyr.pekh@gmail.com>
2016-08-18 14:09:24 -0400

    Another major update

---
+++ version 3/thered/mail.php
@@ -0,0 +1,21 @@
+<?
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+        $to = 'volodymyr.pekh@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
+        $subject = 'Letter from TheRedPin'; //Загаловок сообщения
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+                        <p>Name: '.$_POST['name'].'</p>
+                        <p>Phone: '.$_POST['phone'].'</p> 
+                        <p>E-mail: '.$_POST['email'].'</p>
+                        <p>Message: '.$_POST['message'].'</p>                         
+                    </body>
+                </html>'; //Текст нащего сообщения можно использовать HTML теги
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+        $headers .= "From: TheRedPin <from@example.com>\r\n"; //Наименование и почта отправителя
+        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+}
+?>
---
+++ version 3/thered/male2.php
@@ -0,0 +1,37 @@
+<?
+
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['message'])&&$_POST['message']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+
+        $to = 'volodymyr.pekh@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
+
+        $subject = 'Letter from TheRedPin'; //Загаловок сообщения
+
+        $message = '
+
+                <html>
+
+                    <head>
+
+                        <title>'.$subject.'</title>
+
+                    </head>
+
+                    <body>
+
+                        <p>Name: '.$_POST['name'].'</p>
+
+                        <p>Message: '.$_POST['message'].'</p>                         
+
+                    </body>
+
+                </html>'; //Текст нащего сообщения можно использовать HTML теги
+
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+
+        $headers .= "From: TheRedPin <from@example.com>\r\n"; //Наименование и почта отправителя
+
+        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+
+}
+
+?>
